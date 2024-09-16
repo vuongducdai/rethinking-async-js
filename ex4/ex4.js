1902a68bef3a3bc5c2ec61647855241315eafabc
daivd <daivd@vng.com.vn>
2024-09-16 21:21:33 +0700

    solution for ex4

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -1,36 +1,39 @@
-function fakeAjax(url,cb) {
-	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
-	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+function fakeAjax(url, cb) {
+  var fake_responses = {
+    file1: "The first text",
+    file2: "The middle text",
+    file3: "The last text",
+  };
+  var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
 
-	console.log("Requesting: " + url);
+  console.log("Requesting: " + url);
 
-	setTimeout(function(){
-		cb(fake_responses[url]);
-	},randomDelay);
+  setTimeout(function () {
+    cb(fake_responses[url]);
+  }, randomDelay);
 }
 
 function output(text) {
-	console.log(text);
+  console.log(text);
 }
 
 // **************************************
 // The old-n-busted callback way
 
 function getFile(file) {
-	return new Promise(function(resolve){
-		fakeAjax(file,resolve);
-	});
+  return new Promise(function (resolve) {
+    fakeAjax(file, resolve);
+  });
 }
 
-// Request all files at once in
-// "parallel" via `getFile(..)`.
-//
-// Render as each one finishes,
-// but only once previous rendering
-// is done.
+function promiseAll(listOfFiles) {
+  const listOfPromises = listOfFiles.map(getFile);
 
-// ???
+  listOfPromises.reduce(
+    (accumulator, currentValue) =>
+      accumulator.then(output).then(() => currentValue),
+    Promise.resolve()
+  );
+}
+
+promiseAll(['file1', 'file2', 'file3']);
