ef1ddb8dde300f3bf8312bd55ef5c865396ff24b
daivd <daivd@vng.com.vn>
2024-09-16 07:05:09 +0700

    Solution for ex3

--- ex3/ex3.js
+++ ex3/ex3.js
@@ -1,27 +1,47 @@
-function fakeAjax(url,cb) {
-	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
-	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+function fakeAjax(url, cb) {
+  var fake_responses = {
+    file1: "The first text",
+    file2: "The middle text",
+    file3: "The last text",
+  };
+  var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
 
-	console.log("Requesting: " + url);
+  console.log("Requesting: " + url);
 
-	setTimeout(function(){
-		cb(fake_responses[url]);
-	},randomDelay);
+  setTimeout(function () {
+    cb(fake_responses[url]);
+  }, randomDelay);
 }
 
 function output(text) {
-	console.log(text);
+  console.log(text);
 }
 
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+  // what do we do here?
+  return new Promise((resolve, reject) => {
+    fakeAjax(file, resolve);
+  });
 }
 
 // request all files at once in "parallel"
 // ???
+
+p1 = getFile("file1");
+p2 = getFile("file2");
+p3 = getFile("file3");
+
+p1.then(output)
+  .then(() => {
+    return p2;
+  })
+  .then(output)
+  .then(() => {
+    return p3;
+  })
+  .then(output)
+  .then(() => {
+    output("Completed!");
+  });
